bbb1218124b6ea86d07434010ea117ba8b1cd891
Arman <armanzarei1378@gmail.com>
2021-04-29 12:08:55 +0430

    Add Register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from "axios";
 
 const Register = () => {
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastname] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirm, setPasswordConfirm] = useState("");
+
+    const submit = async (e) => {
+        e.preventDefault();
+
+        const data = {
+            first_name: firstname,
+            last_name: lastname,
+            email,
+            password,
+            password_confirm: passwordConfirm,
+        };
+
+        console.log(data);
+        axios.post('http://localhost:8000/register', data).then(
+            res => {
+                console.log(res.data);
+            }
+        ).catch(err  => {console.log(err)})
+    };
+
     return (
-        <div>
-            Register
-        </div>
+        <form className="form-signin" onSubmit={submit}>
+            <h1 className="h3 mb-3 font-weight-normal">Please register</h1>
+            <input type="text" className="form-control" placeholder="First name" required onChange={e => setFirstname(e.target.value)} />
+            <input type="text" className="form-control" placeholder="Last name" required onChange={e => setLastname(e.target.value)} />
+            <input type="email" className="form-control" placeholder="Email address" required onChange={e => setEmail(e.target.value)} />
+            <input type="password" className="form-control" placeholder="Password" required onChange={e => setPassword(e.target.value)} />
+            <input type="password" className="form-control" placeholder="Password repeat" required onChange={e => setPasswordConfirm(e.target.value)} />
+            <button className="btn btn-primary btn-block" type="submit">Submit</button>
+        </form>
     );
 };
 
