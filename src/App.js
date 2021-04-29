b07a5f8358742ad163d4821a592da4f7ba787bbe
Arman <armanzarei1378@gmail.com>
2021-04-29 10:07:58 +0430

    Add Router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from "react";
 import './App.css';
 import Login from "./pages/Login";
 import Nav from "./components/Nav";
+import {BrowserRouter, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
 
 
 function App() {
   return (
     <div className="App">
-      <Nav/>
+        <BrowserRouter>
+            <Nav/>
 
-      <Login/>
+            <Route path="/" exact component={Home} />
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+        </BrowserRouter>
     </div>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Nav = () => {
     return (
         <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-            <a className="navbar-brand" href="#">Home</a>
+            <Link to="/" className="navbar-brand">Home</Link>
 
             <div className="collapse navbar-collapse" id="navbarCollapse">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
-                        <a className="nav-link" href="#">Login</a>
+                        <Link to="/login" className="nav-link">Login</Link>
                     </li>
                     <li className="nav-item active">
-                        <a className="nav-link" href="#">Register</a>
+                        <Link to="/register" className="nav-link">Register</Link>
                     </li>
                 </ul>
             </div>
