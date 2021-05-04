26d7246c1e69740fdaa1cb8310ff6235bdc5dae0
Arman <armanzarei1378@gmail.com>
2021-05-05 00:08:32 +0430

    Add Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Redirect} from 'react-router-dom';
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [redirect, setRedirect] = useState(false);
+
+    const submit = async (e) => {
+        e.preventDefault();
+
+        const data = {
+            email,
+            password,
+        };
+
+        await fetch('http://127.0.0.1:8000/login', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            credentials: 'include',
+            body: JSON.stringify(data)
+        });
+
+        setRedirect(true);
+    };
+
+    if (redirect) {
+        return <Redirect to="/login"/>
+    }
+
     return (
-        <form className="form-signin">
+        <form className="form-signin" onSubmit={submit}>
             <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-            <input type="email" className="form-control" placeholder="Email address" required />
-            <input type="password" className="form-control" placeholder="Password" required />
+            <input type="email" className="form-control" placeholder="Email address" required onChange={e => setEmail(e.target.value)} />
+            <input type="password" className="form-control" placeholder="Password" required onChange={e => setPassword(e.target.value)} />
             <button className="btn btn-primary btn-block" type="submit">Sign in</button>
         </form>
     );
